4fc0ac10bd249040e04711bee91d682845364482
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-04-11 08:44:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-# Use the official image for Apache HTTP Server with PHP
-FROM php:7.4-apache
-
-# Copy the index.php file to the document root
-COPY index.php /var/www/html/
-
-# Ensure mod_rewrite is enabled for Apache (optional, if needed)
-RUN a2enmod rewrite
-
-# Expose port 80
-EXPOSE 80
-
-# Start Apache in the foreground
-CMD ["apache2-foreground"]
+<?php
+echo 'Instance Private IP: ' . $_SERVER['SERVER_ADDR'];
+?>
